a19baa63c413900bfff98299fb2e69ccffa9f103
“Shubham” <shubhampattar2@gmail.com>
2020-06-11 13:53:26 +0530

    UC3- Added Choose Symbol for Player And Computer after winning toss

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -6,15 +6,39 @@ PLAYER=1;
 SYMBOL1='X';
 SYMBOL2='O';
 
+# VARIABLES
+playerSymbol='';
+ComputerSymbol='';
+
 function play()
 {
 	local TOSS=$((RANDOM%2));
+	local OPTION=0;
 	case $TOSS in
 		0 )
 			echo "You Won the Toss !!! ";
+			echo "Choose Your Symbol..";
+			read -p "press 1 for O, 2 for X." OPTION
+			if [[ $OPTION -eq 1 ]]; 
+			then
+				playerSymbol=$SYMBOL2;
+			elif [[ $OPTION -eq 2 ]]; 
+			then
+				playerSymbol=$SYMBOL1;
+			fi
+			echo "Your Symbol is : "$playerSymbol;
 			;;
 		1 )
 			echo "Computer Won the Toss !!!";
+			OPTION=$(((RANDOM%2)+1));
+			if [[ $OPTION -eq 1 ]]; 
+			then
+				ComputerSymbol=$SYMBOL2;
+			elif [[ $OPTION -eq 2 ]]; 
+			then
+				ComputerSymbol=$SYMBOL1;
+			fi
+			echo "Computer Symbol is : "$ComputerSymbol;
 			;;
 	esac
 }
